5d295de685da68b16129ef787183f3af93cbe1b8
Elbert Nathanael <elbertwithane@gmail.com>
2023-04-16 16:18:41 +0800

    tried cursor offset method. not great

--- src/main.ts
+++ src/main.ts
@@ -14,19 +14,21 @@ const circleRadius = (em(5))/2;
 
 // handle click status
 let circleIsClicked = false;
-window.addEventListener('mouseup', ()=>{circleIsClicked = false;})
-window.addEventListener('touchend', ()=>{circleIsClicked = false;})
-elCircle.addEventListener('mousedown', ()=>{circleIsClicked = true;})
-elCircle.addEventListener('touchstart', ()=>{circleIsClicked = true;})
+window.addEventListener('mouseup', ()=>{circleIsClicked = false; yDelta = 0})
+window.addEventListener('touchend', ()=>{circleIsClicked = false; yDelta = 0})
+elCircle.addEventListener('mousedown', ()=>{circleIsClicked = true; moneyValue = 0})
+elCircle.addEventListener('touchstart', ()=>{circleIsClicked = true; moneyValue = 0})
 
 // lerping
 function lerp(a:number, b:number, t=0.5){
   return a + ((b-a)*t)
 }
 
-// track cursor position
+// track cursor position and y delta
 const currentCirclePosition = {x: 0, y: 0};
 const cursorPosition = {x: 0, y: 0};
+let yDelta = 0;
+const previousCursorPosition = {x:0, y:0};
 window.addEventListener('mousemove', (e)=>{
   cursorPosition.x = e.pageX;
   cursorPosition.y = e.pageY;
@@ -51,9 +53,54 @@ function getDefaultCirclePosition(){
   }
 }
 
+// x position into multiplier
+const screenSpaceMultiplierRows = [0, 0, 0];
+function updatesScreenSpaceMultiplierRows(){
+  const screenWidth = windowSize.x/2;
+  screenSpaceMultiplierRows[0] = screenWidth * (1/3);
+  screenSpaceMultiplierRows[1] = screenWidth * (2/3);
+  screenSpaceMultiplierRows[2] = screenWidth * (3/3);
+}
+updatesScreenSpaceMultiplierRows();
+let multiplier = 0;
+function xIntoMultiplier(){
+  const relativeXPos = cursorPosition.x - (windowSize.x/2)
+  const relativeXPosAbs = Math.abs(relativeXPos)
+  let multiplierId = 0
+  if(relativeXPosAbs < screenSpaceMultiplierRows[0]){multiplierId =  1}
+  else if(relativeXPosAbs < screenSpaceMultiplierRows[1]){multiplierId =  2}
+  else{multiplierId =  3}
+
+  if(relativeXPos < 0){multiplierId *= -1}
+  console.log(multiplierId);
+
+
+  let finalMultiplier = 0
+  switch(multiplierId){
+    case 2: 
+      finalMultiplier = 10;
+      break;
+    case 3: 
+      finalMultiplier = 100;
+      break;
+    case -2: 
+      finalMultiplier = 0.5;
+      break;
+    case -3: 
+      finalMultiplier = 0.1;
+      break;
+    default: 
+      finalMultiplier = 1      
+      break;
+  }
+
+  return finalMultiplier;
+}
+
 
 // processing circle position
 function processCircle(){
+  // handle circle ui
   if(circleIsClicked){
     // lerp click to pointer position
     currentCirclePosition.x = lerp(currentCirclePosition.x, cursorPosition.x)
@@ -65,22 +112,25 @@ function processCircle(){
     currentCirclePosition.y = lerp(currentCirclePosition.y, defaultPosition.y)
     elCircle.setAttribute('style', `top: ${currentCirclePosition.y - circleRadius}px; left: ${currentCirclePosition.x - circleRadius}px;`)
   }
+
+
+  // calculate y delta
+  yDelta = previousCursorPosition.y - cursorPosition.y;
+  // save previous position
+  previousCursorPosition.x = cursorPosition.x;
+  previousCursorPosition.y = cursorPosition.y;
 }
 
 
 // offset into number
-function getCircleOffset(){
-  const defaultPos = getDefaultCirclePosition();
-  const offset = {
-    x: currentCirclePosition.x - defaultPos.x,
-    y: currentCirclePosition.y - defaultPos.y,
-  }
-  return offset
+let moneyValue = 0
+function updateMoneyValue(delta:number, multiplier=2){
+  moneyValue += delta * multiplier;
+  // update ui
+  updateDollars(moneyValue)
 }
-function offsetIntoDollarNumber(){
-  const dollarOffset = -Math.floor(getCircleOffset().y * 0.05);
-  return dollarOffset
-}
+
+// updateing dollar ui
 const elDollarDiv = document.getElementById('dollarDiv');
 function updateDollars(num:number){
   elDollarDiv!.innerHTML = `$${num.toString()}`
@@ -92,8 +142,9 @@ updateDollars(0)
 const update = ()=>{
   processCircle();
 
-  updateDollars(offsetIntoDollarNumber())
-  
+  // process delta into number
+  if(circleIsClicked){updateMoneyValue(yDelta, xIntoMultiplier())};
+
 
   requestAnimationFrame(update);
 }
