66e828e56886f96483fea51b4b546fa10c7d218b
Elbert Nathanael <elbertwithane@gmail.com>
2023-06-01 14:52:08 +0800

    pwa testing

--- src/pwa/initialiseServiceWorker.ts
+++ src/pwa/initialiseServiceWorker.ts
@@ -1,6 +1,6 @@
 export default function initializeServiceWorkers(){
     if('serviceWorker' in navigator){
-      navigator.serviceWorker.register('/PennyPincher/sw.js', {scope:'.'})
+      navigator.serviceWorker.register('/PennyPincher/sw.js?version=1', {scope:'.'})
       .then(()=>{
          console.log('service worker registered');
       });
