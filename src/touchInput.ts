4dc0e69a3a45adefb381493e6a2fc1a0a1d13ef8
elbert <elbertwithane.com>
2024-01-05 12:06:09 +0800

    added functions to generate lines

--- src/touchInput.ts
+++ src/touchInput.ts
@@ -26,6 +26,11 @@ export default class PennyInput{
 	circleRadius = rem(3);
 	// pointer states
 	pointerPos:Vec2 = {x:0, y:0};
+
+	// pointer boundaries and zones
+	horizontalLines:Array<number> = []
+	verticalLines:Array<number> = []
+
 	constructor(){
 		// get elements
 		this.elTouchCanvas = document.getElementById('touchCanvas')!
@@ -40,10 +45,12 @@ export default class PennyInput{
 		window.addEventListener('touchmove', (e)=>{this.pointerPos = {x:e.touches[0].clientX, y:e.touches[0].clientY}})
 		// clicking state
 		this.elCircle.addEventListener('pointerdown', ()=>{this.clicked = true});
-		this.elTouchCanvas.addEventListener('pointerup', ()=>{this.clicked = false})
+		this.elTouchCanvas.addEventListener('pointerup', ()=>{this.clicked = false; this.pointerPos = this.getDefaultPosition()})
+		this.elTouchCanvas.addEventListener('mouseup', ()=>{this.clicked = false; this.pointerPos = this.getDefaultPosition()})
+		this.elTouchCanvas.addEventListener('touchend', ()=>{this.clicked = false; this.pointerPos = this.getDefaultPosition()})
 
 		// update
-		setInterval(this.update.bind(this), 1000/30);
+		setInterval(this.update.bind(this), 1000/20);
 		this.update();
 	}
 	update(){
@@ -61,6 +68,7 @@ export default class PennyInput{
 			x:this.elTouchCanvas.clientWidth,
 			y:this.elTouchCanvas.clientHeight
 		}
+		this.calculateZoneLines();
 	}
 	getDefaultPosition(){
 		const mid:Vec2 = {
@@ -71,5 +79,35 @@ export default class PennyInput{
 	}
 	setCirclePos(pos:Vec2){
 		this.elCircle.setAttribute('style',`top: ${this.circlePosition.y-this.circleRadius/2}px; left:${this.circlePosition.x-this.circleRadius/2}px`)
+	}
+	calculateZoneLines(){
+		// --- generate horizontalLines
+		// start from default position
+		const defaultYPos = this.getDefaultPosition().y
+		this.horizontalLines = [defaultYPos];
+		// add 10 lines
+		for(let i = 0; i < 10; i++){
+			this.horizontalLines.push(defaultYPos + (i+1)*rem(1))
+		}
+		// --- generate vertical lines1
+		// get full size of the x
+		const fullX = this.size.x
+		// create first and last lines
+		const edgeWidthInPx = rem(1)
+		const firstLine = edgeWidthInPx
+		const lastLine = fullX-edgeWidthInPx
+		// create the other 2 lines in the middle
+		const remainingX = this.size.x - edgeWidthInPx*2
+		const middleZoneWidth = remainingX/3
+		// combine it all in one array 
+		this.verticalLines = [
+			firstLine,
+			firstLine + middleZoneWidth,
+			firstLine + middleZoneWidth*2,
+			lastLine
+		]
+		// ! temp logging
+		console.log(this.horizontalLines)
+		console.log(this.verticalLines)
 	}
 }
